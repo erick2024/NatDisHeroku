785396b7a40daba6c1a694f17156017e503ae759
erick2024 <71725468+erick2024@users.noreply.github.com>
2020-11-17 00:02:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ app.layout = html.Div ([
         min = data ['Year'].min(),
         max = data ['Year'].max(),
         value = data ['Year'].max(),
-        marks = {str (year) : {'label' :str (year), 'style' : {'color' : 'blue', 'fontSize' : 9, 'writing-mode' : 'vertical-lr', 'text-orientation' : 'sideways-right'}} for year in data ['Year'].unique()}
+        marks = {str (year) : {'label' : str (year), 'style' : {'color' : 'blue', 'fontSize' : 9, 'writing-mode' : 'vertical-lr', 'text-orientation' : 'sideways-right'}} for year in data ['Year'].unique ()}
     ),
     html.Div (id = 'output-container-slider'),
     ], style = {'display' : 'inline-block', 'width' : '800px'}),      
         html.Div ([
         dcc.Graph (id = 'graph_1'),
-    ], style = {'display' : 'inline-block', 'width' : '200px'}),
+    ], style = {'display' : 'inline-block', 'width' : '60%'}),
      html.Div ([
         dcc.Graph (id = 'graph_2'),
     ], style = {'display' : 'inline-block', 'width' : '40%'})
@@ -61,6 +61,44 @@ def create_graphs (column, value):
       marker_line_color = 'darkgray',
       marker_line_width = 0.5
 ))
-    
-if __name__ == '__main__' :
-  app.run_server ()
+        
+    map.update_layout (
+      title_text = column + ' deaths in '+ str (value) + ' (average: '+ str (average) + ' deaths)',
+      geo = dict (
+          showframe = False,
+          showcoastlines = False,
+          projection_type = 'equirectangular'
+      ), coloraxis_showscale = False,
+      annotations = [dict (
+        x = 0.05,
+        y = 0.05,
+        xref = 'paper',
+        yref = 'paper',
+        text = 'Source:<a href="https://www.emdat.be/">\
+            EM-DAT</a>',
+        showarrow = False
+    )]
+)
+
+    top_15 = filtered_df.sort_values (column, ascending = False)
+
+    bar = px.bar (top_15.head (15), x = 'Country', y = column, color = column, color_continuous_scale = 'Reds')
+
+    bar.update_xaxes (title_text = '')
+    bar.update_yaxes (title_text = '')
+    bar.update_layout (title = 'Top 15 Countries')
+    return map, bar
+
+@app.callback(
+    Output('output-container-slider', 'figure'),
+    [Input ('column', 'value'), Input ('year-slider', 'value')]
+)
+@app.callback(
+    [Output ('graph_1', 'figure'), Output ('graph_2', 'figure')],
+    [Input ('column', 'value'), Input ('year-slider', 'value')]
+)
+def update (column, value):
+    return create_graphs (column, value)
+
+if __name__ == '__main__':
+  app.run_server()
